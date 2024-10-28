42e4454653cfda51f3faed9839019d90f5422017
tomwillow <tomwillow@qq.com>
2024-10-29 00:22:12 +0800

    尝试用unique_ptr管理AVFormatContext，但是报内存泄漏

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,26 @@ std::string GetErrorInfo(int err) noexcept {
     return msg;
 }
 
+class MemoryLeakDetector {
+public:
+    MemoryLeakDetector() noexcept {
+        _CrtMemCheckpoint(&s1);
+    }
+
+    ~MemoryLeakDetector() {
+        _CrtMemState s2;
+        _CrtMemState s3;
+        _CrtMemCheckpoint(&s2);
+        if (_CrtMemDifference(&s3, &s1, &s2)) {
+            _CrtMemDumpStatistics(&s3);
+            assert(0 && "Memory leak is detected!");
+        }
+    }
+
+private:
+    _CrtMemState s1;
+};
+
 class MyFile {
 public:
     MyFile(const std::string &filename, const std::string &mode) : filename(filename) {
@@ -47,22 +67,35 @@ private:
     std::string filename;
 };
 
+class MyStream {
+public:
+    MyStream(AVFormatContext *ctx, int streamIndex) noexcept : ctx(ctx), streamIndex(streamIndex) {}
+
+private:
+    AVFormatContext *ctx;
+    int streamIndex;
+};
+
 class MyAVFormatContext {
 public:
-    MyAVFormatContext(const std::string &filename) {
-        int err = avformat_open_input(&ctx, filename.c_str(), NULL, NULL);
+    MyAVFormatContext(const std::string &filename) : ctx(nullptr, avformat_free_context) {
+        AVFormatContext *rawCtx = nullptr;
+        int err = avformat_open_input(&rawCtx, filename.c_str(), NULL, NULL);
         if (err < 0) {
             throw std::runtime_error(fmt::format("failed to avformat_open_input. err = {}", err));
         }
+
+        ctx.reset(rawCtx);
     }
 
-    ~MyAVFormatContext() {
-        avformat_close_input(&ctx);
+    MyStream FindAudioStream() const {
+        int streamIndex = av_find_best_stream(ctx.get(), AVMediaType::AVMEDIA_TYPE_AUDIO, -1, -1, NULL, NULL);
+        return MyStream(ctx.get(), streamIndex);
     }
 
     void Fun() {
         const AVCodec *codec = NULL;
-        int steamIndex = av_find_best_stream(ctx, AVMediaType::AVMEDIA_TYPE_AUDIO, -1, -1, &codec, NULL);
+        int steamIndex = av_find_best_stream(ctx.get(), AVMediaType::AVMEDIA_TYPE_AUDIO, -1, -1, &codec, NULL);
         if (steamIndex < 0) {
             throw std::runtime_error(fmt::format("failed to av_find_best_stream. err = {}", steamIndex));
         }
@@ -73,7 +106,7 @@ public:
         MyFile file("out.aac", "wb");
 
         while (1) {
-            int err = av_read_frame(ctx, &pkt);
+            int err = av_read_frame(ctx.get(), &pkt);
             if (err < 0) {
                 if (err == AVERROR_EOF) {
                     // spdlog::info("end.");
@@ -96,28 +129,49 @@ public:
     }
 
 private:
-    AVFormatContext *ctx{NULL};
+    std::unique_ptr<AVFormatContext, void (*)(AVFormatContext *)> ctx;
+};
+
+class MyOutputContext {
+public:
+    MyOutputContext(const std::string &outputFileName) : ctx(nullptr, [](AVFormatContext *) {}) {
+        // const char *codecName = avcodec_get_name(ctx->streams[1]->codecpar->codec_id);
+
+        // const AVOutputFormat *outFormat = av_guess_format(NULL, (std::string(".") + codecName).c_str(), NULL);
+        // if (outFormat == nullptr) {
+        //     throw std::runtime_error(fmt::format("failed to av_guess_format. "));
+        // }
+
+        // AVFormatContext *outCtx = nullptr;
+        // err = avformat_alloc_output_context2(&outCtx, outFormat, NULL, NULL);
+        // if (err < 0) {
+        //     throw std::runtime_error(
+        //         fmt::format("failed to avformat_alloc_output_context2. err = {}, {}", err, GetErrorInfo(err)));
+        // }
+
+        //// av_dump_format(outCtx->oformat, 0, "tesss", 1);
+
+        // avformat_free_context(outCtx);
+    }
+
+private:
+    std::unique_ptr<AVFormatContext, void (*)(AVFormatContext *)> ctx;
 };
 
 int main() {
-    _CrtMemState s1;
-    _CrtMemState s2;
-    _CrtMemState s3;
+    // 开启运行时内存检查
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     try {
-        _CrtMemCheckpoint(&s1);
+        av_log_set_level(AV_LOG_ERROR);
+
+        MemoryLeakDetector mld;
+
         MyAVFormatContext ctx("test.mp4");
-        ctx.Fun();
+        // ctx.Fun();
     } catch (const std::runtime_error &err) {
         spdlog::error("{}", err.what());
         return -1;
-    }
-    // memory allocations take place here
-    _CrtMemCheckpoint(&s2);
-
-    if (_CrtMemDifference(&s3, &s1, &s2)) {
-        _CrtMemDumpStatistics(&s3);
-        assert(0);
     }
 
     return 0;
